3842c2c8dce26c19a76783523411336c7e190d27
ELI406 <elijah.federico@msubillings.edu>
2022-04-18 03:02:31 -0600

    updated routes to use a group

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Route::middleware(['auth'])->group(function () {
 
-Route::resource('contacts', ContactController::class)->middleware('auth');
+    Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::resource('contacts', ContactController::class);
 
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+});

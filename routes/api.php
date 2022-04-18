5c189a4dfbfcb30301ed6093873bd1a0f50748ef
ELI406 <elijah.federico@msubillings.edu>
2022-04-18 12:49:50 -0600

    login for token for api call

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Models\Contact;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,6 +25,26 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::middleware('auth:sanctum')->get('/contacts', function (Request $request) {
     return Contact::all();
 });
+
+Route::post('/login/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'error' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    $tokenText = $user->createToken($request->device_name)->plainTextToken;
+    return ['token' => $tokenText];
+});
+
 
 
 
